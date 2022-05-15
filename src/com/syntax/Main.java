87479a9cf8fbac36d82c77f4e444f0743baa2682
Lilly033 <ljiljanajevtinijevic@gmail.com>
2022-05-15 13:29:27 -0700

    1st commit

---
+++ src/com/syntax/Main.java
@@ -0,0 +1,14 @@
+package com.syntax;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        // write your code here
+
+        for (int i = 0; i  <=10; i++) {
+            System.out.print(i+" ");
+
+        }
+    }
+}
